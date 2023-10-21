e1106a8dc7fbdd0dda5cc1f175aff729fc59b4b0
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-10-21 23:54:10 +0530

    built a basic web crawler

--- main.go
+++ main.go
@@ -57,7 +57,70 @@ func crawl(crawl_url string) ([]string, []string) {
 	_ = err
 	return imgs, links
 }
+func crawl_images(crawl_url string)([]string){
+	proxyURL, _ := url.Parse("http://IIT2020060:Satwik..060@172.31.2.4:8080/")
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+	c := colly.NewCollector()
 
+	c.WithTransport(client.Transport)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	var imgs []string
+	c.OnHTML("img", func(h *colly.HTMLElement) {
+		source := h.Attr("src")
+		imgs = append(imgs, source)
+	})
+
+
+	err := c.Visit(crawl_url)
+	_ = err
+	fmt.Println(len(imgs))
+	return imgs
+}
+
+func crawl_links(crawl_url string)([]string){
+	proxyURL, _ := url.Parse("http://IIT2020060:Satwik..060@172.31.2.4:8080/")
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+	c := colly.NewCollector()
+	// max_depth := 1
+
+	c.WithTransport(client.Transport)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	var links []string
+	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+		source := h.Attr("href")
+		// if h.Request.Depth >= max_depth{
+		// 	return
+		// }
+		if strings.HasPrefix(source, "http"){
+			// fmt.Printf("Depth: %d, Link: %s\n", h.Request.Depth, source)
+			links = append(links, source)
+		}
+		// h.Request.Visit(source)
+	})
+
+	err := c.Visit(crawl_url)
+	_ = err
+	fmt.Println(len(links))
+	return links
+}
 func index(w http.ResponseWriter, r *http.Request) {
 	layout := "layout.html"
 	tmpl, err := template.ParseFiles(layout, "search.html")
@@ -66,15 +129,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost{
 		crawl_url := r.FormValue("url")
-		// images, links := crawl(crawl_url)
-		// scraped_data := data{
-		// 	URL: crawl_url,
-		// 	Images : images, 
-		// 	Links : links,
-		// }
-		// _ = scraped_data
 		http.Redirect(w, r, "/data?url=" + crawl_url, http.StatusSeeOther)
-		// tmpl.ExecuteTemplate(w, layout, crawl_url)
 	}
 	tmpl.ExecuteTemplate(w,layout,nil)
 }
@@ -97,7 +152,12 @@ func links_func(w http.ResponseWriter, r *http.Request){
 	_= err
 
 	if r.Method == http.MethodGet{
-		tmpl.ExecuteTemplate(w, layout, nil)
+		query := r.URL.Query()
+		crawl_url := query.Get("url")
+
+		links := crawl_links(crawl_url)
+		// fmt.Println(len(links))
+		tmpl.ExecuteTemplate(w, layout, links)
 	}
 }
 func images_func(w http.ResponseWriter, r *http.Request){
@@ -106,7 +166,12 @@ func images_func(w http.ResponseWriter, r *http.Request){
 	_= err
 
 	if r.Method == http.MethodGet{
-		tmpl.ExecuteTemplate(w, layout, nil)
+		query := r.URL.Query()
+		crawl_url := query.Get("url")
+
+		imgs := crawl_images(crawl_url)
+		fmt.Println(len(imgs))
+		tmpl.ExecuteTemplate(w, layout, imgs)
 	}
 }
 
